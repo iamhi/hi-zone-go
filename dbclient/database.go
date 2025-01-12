0b8872a1f669fffea6ba33815fe2e52e98fdf53f
iamhi <inpactt@gmail.com>
2025-01-12 03:08:56 +0100

    Adding database connection module

---
+++ dbclient/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+var IsConnected = false
+
+func ConnectDB(
+	username string,
+	password string,
+	address string,
+	database string) {
+	dsn := "postgres://" + username + ":" + password + "@" + address + database
+	config, err := pgxpool.ParseConfig(dsn)
+
+	if err != nil {
+		fmt.Errorf("Unable to parse database URL: %v", err)
+	}
+
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.HealthCheckPeriod = 30 * time.Second
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+
+	if err != nil {
+		fmt.Errorf("Unable to connect to the database: %v", err)
+	}
+
+	DB = pool
+	IsConnected = true
+	fmt.Println("Connected to database")
+}
